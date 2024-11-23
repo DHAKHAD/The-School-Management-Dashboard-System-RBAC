ac8ad59bdd96ba0d83f302421cc0c157abbdd044
SUNIL NAGAR <95903110+DHAKHAD@users.noreply.github.com>
2024-11-23 21:13:12 +0530

    Add files via upload

---
+++ components/index.ts
@@ -0,0 +1,15 @@
+import Announcements from "@/components/Announcements";
+import AttendanceChartContainer from "@/components/AttendanceChartContainer";
+import CountChartContainer from "@/components/CountChartContainer";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
+import FinanceChart from "@/components/FinanceChart";
+import UserCard from "@/components/UserCard";
+
+export {
+  Announcements,
+  AttendanceChartContainer,
+  CountChartContainer,
+  EventCalendarContainer,
+  FinanceChart,
+  UserCard,
+};
